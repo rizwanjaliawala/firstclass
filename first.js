1acdc098a3015dc6f57a58d48c213b05895c36fb
rizwanjaliawala <156576823+rizwanjaliawala@users.noreply.github.com>
2024-02-29 19:58:15 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,24 @@
+// Question 1
+// console.log("Hello World");
+// Question 2
+// var n:string = "Rabeesha";
+//var msg:string  = "Would you like to learn some Python Today?";
+// console.log(n ," "+ msg);
+//Question 3
+//var n:string = "Rabeesha";
+//console.log(n.toLowerCase());
+//console.log(n.toUpperCase());
+//Question 4
+// console.log(`Francis Bacon Once Said, "Age apears best in four things, Old win o drink , Old friend to trust , old wood to burn and old authors to read " `);
+// Question 5
+//var famper:string = "Francis Bacon";
+//var qt:string = (`Once Said, "Age apears best in four things, Old win o drink , Old friend to trust , old wood to burn and old authors to read " `);
+//console.log(famper, " " + qt);
+//Question 7
+//console.log("Addition: 5+3",5+3);
+//console.log("Subtraction: 10-2",10-2);
+//console.log("Multiplication: 2 x 4",2*4);
+//console.log("Division: 16/2",16/2);
+//Question 8
+var favnum = 5;
+console.log("My Favourite Number is ", favnum);
